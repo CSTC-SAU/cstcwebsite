6ef211a252923f56ddf8cdb453b1b350e37046bc
Anuj Kumar <anujkharbash2@gmail.com>
2025-06-18 21:08:19 +0530

    minor layout changes on github page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
 
 
   return (
-    <div className="min-h-screen  p-8 md:p-16">
+    <div className="min-h-screen max-w-7xl mx-auto  p-8 md:p-16">
       <h1 className="text-4xl font-bold text-center mb-8">Contact Us</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         
